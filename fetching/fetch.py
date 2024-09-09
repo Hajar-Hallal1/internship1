a5289d4a685ed6788566fa41b058a96d4e0df40b
Hajar-Hallal1 <HajarHallal22@gmail.com>
2024-09-09 22:40:29 +0300

    Update fetch.py

--- fetching/fetch.py
+++ fetching/fetch.py
@@ -1,1 +1,13 @@
+import requests
+import pandas as pd
+import json
 
+url = "http://api.openweathermap.org/data/2.5/weather?&lat=33.3789&lon=35.4839&units=metric&lang=en&appid=a2b2e442aba806b89cc7e799526a1158"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+
+with open('weather.csv', 'a') as weather-data:
+    data1 = json.load(weather-data)
+normalized-data = pd.json-normalize(data1)
+df = pd.DataFrame(normalized-data)
